24fcc5b3abcd26d244435fcc3a97b4e125e67f79
Pisei <limpisei@gmail.com>
2016-08-24 15:27:59 -0700

    add sound

--- js/app.js
+++ js/app.js
@@ -7,10 +7,20 @@ $(document).ready(function() {
     $('.ryu-still').show();
   }).mousedown(function(){
     // play hadouken sound
+    playHadouken();
+    
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').show();
+
     // show hadouken and animate it to the right of the screen
+    $('.hadouken').finish().show().animate(
+      {'left': '1020px'},
+      500,
+      function() {
+        $('.hadouken').hide();
+        $('.hadouken').css('left', '520px');
+      }
+    );
   }).mouseup(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
@@ -18,3 +28,8 @@ $(document).ready(function() {
   });
 
 });
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
